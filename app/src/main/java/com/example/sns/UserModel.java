be731efaf72664de84e5c5bc767ae971f57b806e
admin <kdigital.fdb@gmail.com>
2023-01-31 12:40:57 +0900

    sns_fdb

---
+++ app/src/main/java/com/example/sns/UserModel.java
@@ -0,0 +1,11 @@
+package com.example.sns;
+
+/**
+ * Created by myeongsic on 2017. 8. 14..
+ */
+
+public class UserModel {
+    public String userName;
+    public String profileImageUrl;
+    public String uid;
+}
