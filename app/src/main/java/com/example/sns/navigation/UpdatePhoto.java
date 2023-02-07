2a08cdd8ca8b86617bdd04c7494e9d640c0e0f4b
kim <kth07030@naver.com>
2023-02-07 16:10:07 +0900

    agsdg

--- app/src/main/java/com/example/sns/navigation/UpdatePhoto.java
+++ app/src/main/java/com/example/sns/navigation/UpdatePhoto.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.sns.MainActivity;
 import com.example.sns.R;
 import com.example.sns.User;
@@ -52,6 +55,7 @@ public class UpdatePhoto extends AppCompatActivity {
     private SimpleDateFormat timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss");
     public ArrayList<String> contentUidList = new ArrayList<>();
     private String imageFileName ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -64,6 +68,29 @@ public class UpdatePhoto extends AppCompatActivity {
         updatephoto_btn =findViewById(R.id.updatephoto_btn);
         updatephoto_edit=findViewById(R.id.updatephoto_edit);
         updatephoto_img=findViewById(R.id.updatephoto_img);
+/* 수정시 기존값 가져오는코드
+        try {
+            Intent secondIntent = getIntent();
+            int position =secondIntent.getIntExtra("position",0);
+            firestore.collection("images").document(contentUidList.get(position)).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot value) {
+                    String updateedit = String.valueOf(value.getData().get("explain"));
+                    String str = String.valueOf(value.getData().get("imageUri"));
+                    Log.e("수정",updateedit);
+                    Log.e("수정",str);
+                    updatephoto_edit.setText(updateedit);
+                    Uri uri = Uri.parse(str);
+                    Glide.with(UpdatePhoto.this)
+                            .load(uri)
+                            .apply(RequestOptions.circleCropTransform())
+                            .into(updatephoto_img);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ */
 
         uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         firestore.collection("images").orderBy("timestamp").get()
@@ -81,7 +108,6 @@ public class UpdatePhoto extends AppCompatActivity {
                         }
                     }
                 });
-
         updatephoto_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,6 +122,7 @@ public class UpdatePhoto extends AppCompatActivity {
                 updatePhoto();
             }
         });
+
 
     }
     @Override
@@ -143,7 +170,7 @@ public class UpdatePhoto extends AppCompatActivity {
                         update.put("timestamp",timestamp);
                         update.put("explain",explain);
                         Intent secondIntent = getIntent();
-                        int position =secondIntent.getIntExtra("position",0);
+                        int position =secondIntent.getIntExtra("position",1);
                         firestore.collection("images").document(contentUidList.get(position)).update(update).addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
